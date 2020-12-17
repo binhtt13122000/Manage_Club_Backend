35bfbc06ad6888666bd4ffd71ca96745b5dbb02c
BinhTT1312 <binhtt.13122000@gmail.com>
2020-12-17 14:49:49 +0700

    create bean for service

---
+++ src/main/java/binhtt/dev/manage/services/imp/AccountServiceImp.java
@@ -0,0 +1,8 @@
+package binhtt.dev.manage.services.imp;
+
+import binhtt.dev.manage.services.AccoutService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountServiceImp implements AccoutService {
+}
