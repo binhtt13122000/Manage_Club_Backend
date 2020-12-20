05e1e9da714fc213b28e38dada386c59f6863afb
BinhTT1312 <binhtt.13122000@gmail.com>
2020-12-20 15:33:44 +0700

    add constaint to Account

--- src/main/java/binhtt/dev/manage/entities/Account.java
+++ src/main/java/binhtt/dev/manage/entities/Account.java
@@ -1,6 +1,7 @@
 package binhtt.dev.manage.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -10,29 +11,58 @@ import java.io.Serializable;
 @Entity
 @Table(name = "tbl_user")
 @Data
+@Schema(name = "Account")
 public class Account implements Serializable {
+    //studentId
     @Id
     @Column(name = "studentID", nullable = false, unique = true, updatable = false)
+    @NotNull
+    @NotBlank
+    @Size(max = 8, min = 8)
+    @Schema(example = "SE140125")
     private String studentID;
+    //password
     @JsonIgnore
     @Column(name = "password", nullable = false)
     private String password;
+    //fullname
     @Column(name = "fullname", nullable = false)
-    private @NotNull @NotBlank String fullname;
+    @NotNull
+    @NotBlank
+    @Size(max = 50)
+    @Schema(example = "Trương Thanh Bình")
+    private String fullname;
+    //avatar
     @Column(name = "avatarId")
     private String avatarId;
+    //phone
     @Column(name = "phone")
-    private @Max(10) String phone;
+    @Size(max = 10, min = 9)
+    @Schema(example = "0335579880")
+    private String phone;
+    //email
     @Column(name = "email", unique = true)
-    private @NotNull @NotBlank @Email String email;
+    @NotNull
+    @NotBlank
+    @Email
+    @Size(max = 50)
+    @Schema(example = "binhttse140125@fpt.edu.vn")
+    private String email;
+    //status
     @Column(name = "status")
+    @Schema(example = "true")
     private boolean status;
+
+    //relation
     @ManyToOne
     @JoinColumn(name = "roleId", nullable = false)
     @JsonIgnore
     private Role role;
 
+    //role
     @Transient
+    @NotNull
+    @Schema(example = "2")
     private int roleId;
 
     public int getRoleId() {
